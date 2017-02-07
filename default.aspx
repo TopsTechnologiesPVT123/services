ae864f0cb67d1ef2fe51606785cefb4331938077
TopsTechnologiesPVT123 <bhuyangdevhost@gmail.com>
2017-02-07 11:56:44 +0530

    hi

---
+++ default.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="ServiceOrientedApplication._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" type="text/javascript"></script>
+            <script type="text/javascript">
+                function CallMethods() {
+                    $.ajax({
+                        type: "POST",
+                        url: "default.aspx/login",
+                        data: JSON.stringify({ "uname": "a", "password": "123" }),
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (data) { alert(data.d); },
+                        failure: function (response) {
+                            alert(response.d);
+                        }
+                    });
+                }
+
+                CallMethods();
+            </script>
+        </div>
+    </form>
+</body>
+</html>
---
+++ default.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace ServiceOrientedApplication
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        public class person
+        {
+            public int age;
+            public string name;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [WebMethod()]
+        public static List<string> methodCall()
+        {
+            List<string> str = new List<string>();
+            str.Add("a1");
+            str.Add("a2");
+            str.Add("a3");
+            str.Add("a4");
+            return str;
+        }
+
+        [WebMethod()]
+        public static List<person> getPerson()
+        {
+            List<person> personObj = new List<person>();
+            personObj.Add(new person() { age = 10, name = "abc" });
+            personObj.Add(new person() { age = 10, name = "abc" });
+            personObj.Add(new person() { age = 10, name = "abc" });
+            personObj.Add(new person() { age = 10, name = "abc" });
+            return personObj;
+        }
+
+        [WebMethod()]
+        public static string getAll()
+        {
+            JavaScriptSerializer TheSerializer = new JavaScriptSerializer();
+
+            //optional: you can create your own custom converter
+            //TheSerializer.RegisterConverters(new JavaScriptConverter[] { new person() });
+            List<person> personObj = new List<person>();
+            personObj.Add(new person() { age = 10, name = "abc" });
+            personObj.Add(new person() { age = 10, name = "abc" });
+            personObj.Add(new person() { age = 10, name = "abc" });
+            personObj.Add(new person() { age = 10, name = "abc" });
+            var products = personObj.ToList();
+
+            var TheJson = TheSerializer.Serialize(products);
+
+            return TheJson;
+        }
+
+        [WebMethod()]
+        public static string login(string uname, string password)
+        {
+            JavaScriptSerializer TheSerializer = new JavaScriptSerializer();
+
+            responseClass res = new responseClass();
+
+            if (uname == "a" && password == "123")
+            {
+
+                res.status= "1";
+                res.message = "done";
+
+
+
+            }
+            else
+            {
+                res.status = "-1";
+                res.message = "err";
+            }
+            var data = res;
+
+            var TheJson = TheSerializer.Serialize(data);
+            return TheJson;
+        }
+
+        public class responseClass {
+            public string status { get; set; }
+            public string message { get; set; }
+        }
+
+
+        
+    }
+}
